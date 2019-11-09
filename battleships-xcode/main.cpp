3cfa9df1e4ea272298c6420dfe7b066fa490908c
rgdl <robert_delisle44@hotmail.com>
2019-11-09 21:27:48 +1100

    Initial Commit

---
+++ battleships-xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  battleships-xcode
+//
+//  Created by Robert de Lisle on 9/11/19.
+//  Copyright © 2019 Robert de Lisle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
